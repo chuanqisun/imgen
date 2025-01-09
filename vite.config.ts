0176735140699852d19b626513ead4c18c6b9b16
chuanqisun <chuanqi.sun.dev@gmail.com>
2025-01-09 10:28:01 -0800

    rename project

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/imgen", // align with the GitHub Pages repository nameq
+  base: "/sixml/", // align with the GitHub Pages repository nameq
 });
