815b8d041103ee9473476b30f06c75ae08c3ee74
chuanqisun <chuanqi.sun.dev@gmail.com>
2025-01-09 10:21:22 -0800

    clean up connection form

--- src/lib/ai-bar/lib/elements/azure-connection.ts
+++ src/lib/ai-bar/lib/elements/azure-connection.ts
@@ -34,30 +34,31 @@ export class AoaiConnectionButton extends HTMLElement implements AzureConnection
       <input type="url" id="aoai-endpoint" name="aoai-endpoint"
         placeholder="https://replace-endpoint-name.openai.azure.com/" />
 
-      <label for="aoai-deployment-name">AOAI Deployment Name</label>
-      <input type="text" id="aoai-deployment-name" name="aoai-deployment-name" placeholder="my-gpt-4o" />
-
       <label for="aoai-key">AOAI Key</label>
       <input type="password" id="aoai-key" name="aoai-key" />
 
       <label for="openai-key">OpenAI Key</label>
       <input type="password" id="openai-key" name="openai-key" />
 
-      <label for="googleai-key">Google AI Key</label>
-      <input type="password" id="googleai-key" name="googleai-key" />
-
-      <label for="speech-region">Speech region</label>
+      <label for="speech-region">Azure Speech region</label>
       <input type="text" id="speech-region" name="speech-region" placeholder="eastus" />
 
-      <label for="speech-key">Speech key</label>
+      <label for="speech-key">Azure Speech key</label>
       <input type="password" id="speech-key" name="speech-key" />
-
-      <label for="eleven-labs-key">ElevenLabs key</label>
-      <input type="password" id="eleven-labs-key" name="eleven-labs-key" />
-
 
       <label for="together-ai-key">TogetherAI key</label>
       <input type="password" id="together-ai-key" name="together-ai-key" />
+
+      <!--
+      <label for="eleven-labs-key">ElevenLabs key</label>
+      <input type="password" id="eleven-labs-key" name="eleven-labs-key" />
+
+      <label for="aoai-deployment-name">AOAI Deployment Name</label>
+      <input type="text" id="aoai-deployment-name" name="aoai-deployment-name" placeholder="my-gpt-4o" />
+
+
+      <label for="googleai-key">Google AI Key</label>
+      <input type="password" id="googleai-key" name="googleai-key" />
 
       <label for="map-key">Azure Map Key</label>
       <input type="password" id="map-key" name="map-key" />
@@ -68,6 +69,7 @@ export class AoaiConnectionButton extends HTMLElement implements AzureConnection
 
       <label for="aoai-key-2">AOAI Key 2</label>
       <input type="password" id="aoai-key-2" name="aoai-key-2" />
+      -->
     </div>
     <button>Done</button>
   </form>
@@ -103,19 +105,19 @@ export class AoaiConnectionButton extends HTMLElement implements AzureConnection
   }
 
   public getAzureConnection() {
-    const aoaiEndpoint = this.shadowRoot.querySelector<HTMLInputElement>("#aoai-endpoint")!.value;
-    const aoaiDeploymentName = this.shadowRoot.querySelector<HTMLInputElement>("#aoai-deployment-name")!.value;
-    const aoaiKey = this.shadowRoot.querySelector<HTMLInputElement>("#aoai-key")!.value;
-    const openaiKey = this.shadowRoot.querySelector<HTMLInputElement>("#openai-key")!.value;
-    const togetherAIKey = this.shadowRoot.querySelector<HTMLInputElement>("#together-ai-key")!.value;
-    const googleAIKey = this.shadowRoot.querySelector<HTMLInputElement>("#googleai-key")!.value;
-    const mapKey = this.shadowRoot.querySelector<HTMLInputElement>("#map-key")!.value;
-    const speechRegion = this.shadowRoot.querySelector<HTMLInputElement>("#speech-region")!.value;
-    const speechKey = this.shadowRoot.querySelector<HTMLInputElement>("#speech-key")!.value;
-    const elevenLabsKey = this.shadowRoot.querySelector<HTMLInputElement>("#eleven-labs-key")!.value;
+    const aoaiEndpoint = this.shadowRoot.querySelector<HTMLInputElement>("#aoai-endpoint")?.value ?? "";
+    const aoaiDeploymentName = this.shadowRoot.querySelector<HTMLInputElement>("#aoai-deployment-name")?.value ?? "";
+    const aoaiKey = this.shadowRoot.querySelector<HTMLInputElement>("#aoai-key")?.value ?? "";
+    const openaiKey = this.shadowRoot.querySelector<HTMLInputElement>("#openai-key")?.value ?? "";
+    const togetherAIKey = this.shadowRoot.querySelector<HTMLInputElement>("#together-ai-key")?.value ?? "";
+    const googleAIKey = this.shadowRoot.querySelector<HTMLInputElement>("#googleai-key")?.value ?? "";
+    const mapKey = this.shadowRoot.querySelector<HTMLInputElement>("#map-key")?.value ?? "";
+    const speechRegion = this.shadowRoot.querySelector<HTMLInputElement>("#speech-region")?.value ?? "";
+    const speechKey = this.shadowRoot.querySelector<HTMLInputElement>("#speech-key")?.value ?? "";
+    const elevenLabsKey = this.shadowRoot.querySelector<HTMLInputElement>("#eleven-labs-key")?.value ?? "";
 
-    const aoaiEndpoint2 = this.shadowRoot.querySelector<HTMLInputElement>("#aoai-endpoint-2")!.value;
-    const aoaiKey2 = this.shadowRoot.querySelector<HTMLInputElement>("#aoai-key-2")!.value;
+    const aoaiEndpoint2 = this.shadowRoot.querySelector<HTMLInputElement>("#aoai-endpoint-2")?.value ?? "";
+    const aoaiKey2 = this.shadowRoot.querySelector<HTMLInputElement>("#aoai-key-2")?.value ?? "";
 
     return {
       mapKey,
