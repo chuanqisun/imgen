69d8cd289072d2779bb2f6870c3b84efe242b595
chuanqisun <chuanqi.sun.dev@gmail.com>
2025-01-09 10:55:51 -0800

    clean up file names

--- src/advanced.ts
+++ src/advanced.ts
@@ -1,6 +1,7 @@
 import { merge } from "rxjs";
 import { defineCodeEditorElement } from "./code-editor/code-editor-element";
 import { loadAIBar } from "./lib/ai-bar/loader";
+import { useDefaultInput } from "./lib/sub-systems/default-input";
 import { useDictateInput } from "./lib/sub-systems/dictate-input";
 import { useDiscussionOutput } from "./lib/sub-systems/discussion-output";
 import { useInterviewInput } from "./lib/sub-systems/interview-input";
@@ -10,9 +11,8 @@ import { useDelegatedPushToTalk, useMicrophone } from "./lib/sub-systems/shared"
 import { useShowInput } from "./lib/sub-systems/show-input";
 import { useWritingOutput } from "./lib/sub-systems/writing-output";
 
-import { useDefaultInput } from "./lib/sub-systems/default-input";
+import "./advanced.css";
 import "./main.css";
-import "./representation.css";
 
 loadAIBar();
 defineCodeEditorElement();
