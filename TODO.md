9b997b65b7469d3c234102ed55c43aab0d05cee2
chuanqisun <chuanqi.sun.dev@gmail.com>
2025-01-08 19:01:28 -0800

    add memory loader

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# TODOs
+
+- Gossip output
+- Camera input
+- Save/Load memory frame
+- Editable memory
+- Prettier memory
+- Scene XML transform before NLP render
+- Markdown to HTML render
--- src/lib/sub-systems/memory.ts
+++ src/lib/sub-systems/memory.ts
@@ -7,6 +7,27 @@ export function useMemory() {
   const forgetButton = $<HTMLButtonElement>("#forget")!;
   const renderXML$ = currentWorldXML.pipe(tap((xml) => (xmlPreview.textContent = xml)));
   const forget$ = fromEvent(forgetButton, "click").pipe(tap(() => currentWorldXML.next(EMPTY_XML)));
+  const saveButton = $<HTMLButtonElement>("#save")!;
+  const loadButton = $<HTMLButtonElement>("#load")!;
+
+  saveButton.addEventListener("click", () => {
+    window.showSaveFilePicker().then((fileHandle) => {
+      fileHandle.createWritable().then((writable) => {
+        writable.write(currentWorldXML.value);
+        writable.close();
+      });
+    });
+  });
+
+  loadButton.addEventListener("click", () => {
+    window.showOpenFilePicker().then((fileHandle) => {
+      fileHandle[0].getFile().then((file) => {
+        file.text().then((text) => {
+          currentWorldXML.next(text);
+        });
+      });
+    });
+  });
 
   return merge(renderXML$, forget$);
 }
